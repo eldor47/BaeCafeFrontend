2effd04cb24b62137930e5c3b3ac55cd3a9185b7
eldor47 <eldor474747@gmail.com>
2022-07-14 08:53:28 -0700

    Fixes image cdn

--- src/components/helper.tsx
+++ src/components/helper.tsx
@@ -1,4 +1,4 @@
-export const CLOUDFRONT_IMG = 'https://d28u60wydcv7iq.cloudfront.net/baecafeweb';
+export const CLOUDFRONT_IMG = 'https://dx8cklxaufs1v.cloudfront.net/baecafeweb';
 export const NETWORK = 'https://etherscan.io/';
 export const OPENSEA = 'https://opensea.io/';
 export const CONTRACT_NUMBER = '0x71663f5fb424e2e9e2c82d86f207d50c8ba481c6';
